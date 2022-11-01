d485136f34555cf60ed58cbbbf6cdd0b0a0f01eb
Guilherme Dantas <guilhermedantas788@gmail.com>
2022-11-01 15:35:32 -0300

    fix bug

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -27,7 +27,7 @@ export function Transactions() {
             {transactions.map((transaction) => {
               return (
                 <tr key={transaction.id}>
-                  <td width="40%">{transaction.description}</td>
+                  <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
                       {transaction.type === 'outcome' && '- '}
