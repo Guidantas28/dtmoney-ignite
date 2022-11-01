91bfe3faa3e723bbd0b62b9c8959e2b1501d891a
Guilherme Dantas <guilhermedantas788@gmail.com>
2022-10-31 21:01:47 -0300

    almost finished, just need to fix one bug about the numbers

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,55 +1,49 @@
-import { useEffect, useState } from "react";
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { SearchForm } from "./components/SearchForm";
-import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
-
-interface Transaction {
-    id: number;
-    description: string;
-    type: 'income' | 'outcome';
-    price: number;
-    category: string;
-    createdAt: string;
-}
+import { useContextSelector } from 'use-context-selector'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-    
-    async function loadTransactions() {
-        const response = await fetch('http://localhost:3333/transactions')
-        const data = await response.json()
-    
-        setTransactions(data)
-      }
-    
-      useEffect(() => {
-        loadTransactions()
-      }, []);
-    
-    return(
-        <div>
-            <Header />
-            <Summary />
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
-            <TransactionsContainer>
-                <SearchForm />
-            <TransactionsTable>
-                <tbody>
-                    {transactions.map(transaction => {
-                       return (
-                        <tr key={transaction.id}>
-                        <td width="40%">{transaction.description}</td>
-                        <td><PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight></td>
-                        <td>{transaction.category}</td>
-                        <td>{transaction.createdAt}</td>
-                    </tr>
-                       )
-                    })}
-                    
-                </tbody>
-            </TransactionsTable>
-            </TransactionsContainer>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="40%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
 }
